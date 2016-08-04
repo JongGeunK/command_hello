45941c7e7cbc74fa035430e1885174e157617eae
JongGeun Kim <mereblue@eugenes.co.kr>
2016-08-04 15:07:46 +0900

    첫번째 commit. -- 파일 최초 추가

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+import com.sun.java;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+	// Prints "Hello, World" to the terminal window.
+	            System.out.println("Hello, World");
+    }
+
+}
